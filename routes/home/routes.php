d29acc5da0c92b7ee51c40fa996ec1bf055be916
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-05-10 00:37:43 +0100

    Middleware is done

--- routes/home/routes.php
+++ routes/home/routes.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Middleware\OnlyUserLoged;
+use App\Http\Middleware\OnlyUserNotLoged;
 use App\Livewire\Home\DashboardComponent;
 use App\Livewire\Home\IndexComponent;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', IndexComponent::class)->name("home.index");
-Route::get('/painel-principal', DashboardComponent::class)->name("home.dashboard");
+Route::get('/', IndexComponent::class)->name("home.index")->middleware(OnlyUserNotLoged::class);
+Route::get('/painel-principal', DashboardComponent::class)->name("home.dashboard")->middleware(OnlyUserLoged::class);
--- routes/pages/routes.php
+++ routes/pages/routes.php
@@ -1,12 +1,12 @@
 <?php
 
-
+use App\Http\Middleware\OnlyUserNotLoged;
 use App\Livewire\Pages\LoginComponent;
 use App\Livewire\Pages\ErrorComponent;
 use App\Livewire\Pages\SignupComponent;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/autenticação', LoginComponent::class)->name("pages.login");
-Route::get('/conta/criar', SignupComponent::class)->name("pages.signup");
+Route::get('/autenticação', LoginComponent::class)->name("pages.login")->middleware(OnlyUserNotLoged::class);
+Route::get('/conta/criar', SignupComponent::class)->name("pages.signup")->middleware(OnlyUserNotLoged::class);
 Route::fallback(ErrorComponent::class);
