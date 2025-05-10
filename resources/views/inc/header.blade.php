c4bc21ddb9509ac74df34ec822c64f84db6774d4
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-05-10 01:59:17 +0100

    head info changes

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,7 +1,7 @@
 <!-- Navbar Start -->
 <nav class="navbar navbar-expand bg-secondary navbar-dark sticky-top px-4 py-0">
-    <a href="index.html" class="navbar-brand d-flex d-lg-none me-4">
-        <h2 class="text-primary mb-0"><i class="fa fa-user-edit"></i></h2>
+    <a href="/" class="navbar-brand d-flex d-lg-none me-4">
+        <h2 class="text-primary mb-0"><img style="width: 40px" src="{{asset('assets/images/logo/redlogo.png')}}" alt=""></h2>
     </a>
     <a href="#" class="sidebar-toggler flex-shrink-0">
         <i class="fa fa-bars"></i>
--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -23,34 +23,42 @@
                 </div>
             </div>
             <div class="ms-3">
-                <h6 class="mb-0">{{ ucwords(auth()->user()->first_name) }} {{ ucwords(auth()->user()->last_name) }}
+                <h6 class="mb-0">{{ ucwords(auth()->user()->first_name) }}  
                 </h6>
                 <span>{{ ucwords(auth()->user()->getAcess->description) }}</span>
             </div>
         </div>
         <div class="navbar-nav w-100">
-            <a href="/" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
+            <a href="/" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Painel</a>
+            <a href="widget.html" class="nav-item nav-link"><i class="fa fa-user me-2"></i>Perfil</a>
+            <a href="form.html" class="nav-item nav-link"><i class="fa fa-home me-2"></i>Imóveis</a>
+            <a href="form.html" class="nav-item nav-link"><i class="fa fa-search me-2"></i>Localizar</a>
+            <a href="table.html" class="nav-item nav-link"><i class="fa fa-list me-2"></i>Pedidos</a>
             <div class="nav-item dropdown">
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i
-                        class="fa fa-laptop me-2"></i>Elements</a>
+                        class="fa fa-file-alt  me-2"></i>Documentos</a>
                 <div class="dropdown-menu bg-transparent border-0">
-                    <a href="button.html" class="dropdown-item">Buttons</a>
-                    <a href="typography.html" class="dropdown-item">Typography</a>
-                    <a href="element.html" class="dropdown-item">Other Elements</a>
+                    <a href="button.html" class="dropdown-item">Registrados</a>
+                    <a href="typography.html" class="dropdown-item">Contratos</a>
                 </div>
             </div>
-            <a href="widget.html" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Widgets</a>
-            <a href="form.html" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Forms</a>
-            <a href="table.html" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Tables</a>
-            <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Charts</a>
             <div class="nav-item dropdown">
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i
-                        class="far fa-file-alt me-2"></i>Pages</a>
+                        class="fa fa-map me-2"></i>Localidades</a>
                 <div class="dropdown-menu bg-transparent border-0">
-                    <a href="signin.html" class="dropdown-item">Sign In</a>
-                    <a href="signup.html" class="dropdown-item">Sign Up</a>
-                    <a href="404.html" class="dropdown-item">404 Error</a>
-                    <a href="blank.html" class="dropdown-item">Blank Page</a>
+                    <a href="button.html" class="dropdown-item">Províncias</a>
+                    <a href="typography.html" class="dropdown-item">Municípios</a>
+                    <a href="element.html" class="dropdown-item">Endereço (Bairro/Rua)</a>
+                </div>
+            </div>
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i
+                        class="fa fa-users me-2"></i>Entidades</a>
+                <div class="dropdown-menu bg-transparent border-0">
+                    <a href="signin.html" class="dropdown-item">Admin</a>
+                    <a href="signup.html" class="dropdown-item">Inquilinos</a>
+                    <a href="404.html" class="dropdown-item">Arrendadores</a>
+                    <a href="blank.html" class="dropdown-item">Intermediários</a>
                 </div>
             </div>
         </div>
