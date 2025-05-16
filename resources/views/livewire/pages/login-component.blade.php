2785e4754737ac092cabf1012552897f149cf063
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-05-16 07:15:46 +0100

    Login and Signup is done

--- resources/views/livewire/pages/login-component.blade.php
+++ resources/views/livewire/pages/login-component.blade.php
@@ -1,17 +1,19 @@
 @section('title', 'Autenticação')
-<div class="container-fluid" style="background: rgba(200,200,200,50)">
-    <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
+<div class="container-fluid" >
+    <div class="row h-100 align-items-center justify-content-center" 
+    style="min-height: 100vh;">
         <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
-            <div class="rounded p-4 p-sm-5 my-4 mx-3 card-animated" style="background: whitesmoke">
+            <div class="bg-white rounded p-4 p-sm-5 my-4 mx-3">
                 <div class="d-flex align-items-center justify-content-between mb-3">
                     <a href="/" class="">
-                        <h3 class="text-primary">@include('inc.namewebsite')</h3>
+                        <h3 class="text-dark">@include('inc.namewebsite')</h3>
                     </a>
-                    <h3 class="text-secondary">Entrar</h3>
-                </div>
+                    <h3 class="text-primary">Entrar</h3>
+                </div> <hr>
                 <div class="form-floating mb-3">
-                    <input type="email" wire:model='emailphone' class="form-control" 
-                    id="floatingInput" style="background: whitesmoke"
+                    <input type="email" wire:model='emailphone' 
+                    class="form-control bg-transparent" style="border: 0.5px solid #222;" 
+                    id="floatingInput"
                         placeholder="name@example.com">
                     <label for="floatingInput">Email ou Telefone</label>
                     @error('emailphone')
@@ -19,8 +21,9 @@
                     @enderror
                 </div>
                 <div class="form-floating mb-4">
-                    <input type="password" wire:model='password' class="form-control" 
-                    id="floatingPassword" style="background: whitesmoke"
+                    <input type="password" wire:model='password' 
+                    class="form-control bg-transparent" style="border: 0.5px solid #222;" 
+                    id="floatingPassword"
                         placeholder="Password">
                     <label for="floatingPassword">Senha</label>
                     @error('password')
@@ -29,18 +32,19 @@
                 </div>
                 <div class="d-flex align-items-center justify-content-between mb-4">
                     <div class="form-check">
-                        <input type="checkbox" wire:model="remember_me" class="form-check-input " id="exampleCheck1">
-                        <label class="form-check-label text-dark" for="exampleCheck1">Lembrar-me</label>
+                        <input type="checkbox" wire:model="remember_me" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Lembrar-me</label>
                     </div>
-                    <a href="">Esqueceu a Senha?</a>
+                    <a href="" class="text-primary fw-bold">Esqueceu a Senha?</a>
                 </div>
-                <button wire:click.prevent='authenticate' type="submit" class="btn btn-dark py-3 w-100 mb-4" wire:loading.attr="disabled"
+                <button wire:click.prevent='authenticate' type="submit" class="btn btn-primary py-3 w-100 mb-4" wire:loading.attr="disabled"
                     wire:target="authenticate">
                     <span wire:loading.remove wire:target="authenticate">Entrar</span>
                     <span wire:loading wire:target="authenticate">
                         <i class="fa fa-spinner fa-spin"></i> 
                     </span></button>
-                <p class="text-center mb-0">Não tem uma conta? <a href="{{route('pages.signup')}}">Cadastre-se</a></p>
+                <p class="text-center mb-0">Não tem uma conta? 
+                    <a href="{{route('pages.signup')}}" class="text-primary fw-bold">Cadastre-se</a></p>
             </div>
         </div>
     </div>
--- resources/views/livewire/pages/signup-component.blade.php
+++ resources/views/livewire/pages/signup-component.blade.php
@@ -2,19 +2,23 @@
 <div class="container-fluid">
     <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
         <div class="col-12 col-sm-8 col-md-6 col-lg-8">
-            <div class="bg-secondary rounded p-4 p-sm-5 my-4 mx-3">
+            <div class="bg-white rounded p-4 p-sm-5 my-4 mx-3">
                 <div class="d-flex align-items-center justify-content-between mb-3">
                     <a href="/" class="">
                         <h3 class="text-primary">@include('inc.namewebsite')</h3>
                     </a>
-                    <h3>Criar Conta</h3>
+                    <h3 class="text-primary">Criar Conta</h3>
                 </div>
+                <hr>
 
                 <form class="container" wire:submit.prevent="save">
                     <div class="row" style="width: inherit">
                         <div class="col-md-6 form-floating mb-3">
                             <input type="text" wire:model="first_name" class="form-control" id="first_name"
-                                placeholder="Nome">
+                            style="border: 0.5px solid #222; 
+                            color: black;
+                            background: transparent"
+                            placeholder="Nome">
                             <label class="ms-2" for="first_name">Nome</label>
                             @error('first_name')
                                 <span class="text-danger mt-2">{{ $message }}</span>
@@ -23,7 +27,10 @@
 
                         <div class="col-md-6 form-floating mb-3">
                             <input type="text" wire:model="last_name" class="form-control" id="last_name"
-                                placeholder="Sobrenome">
+                            style="border: 0.5px solid #222; 
+                            color: black;
+                            background: transparent"
+                            placeholder="Sobrenome">
                             <label class="ms-2" for="last_name">Sobrenome</label>
                             @error('last_name')
                                 <span class="text-danger mt-2">{{ $message }}</span>
@@ -31,7 +38,11 @@
                         </div>
 
                         <div class="col-md-6 form-floating mb-3">
-                            <select wire:model="gender" class="form-select" id="gender">
+                            <select wire:model="gender" 
+                            style="border: 0.5px solid #222; 
+                            color: black;
+                            background: transparent"
+                            class="form-select" id="gender">
                                 <option value="">Selecione</option>
                                 <option value="Masculino">Masculino</option>
                                 <option value="Feminino">Feminino</option>
@@ -44,7 +55,10 @@
 
                         <div class="col-md-6 form-floating mb-3">
                             <input type="date" wire:model="birth_date" class="form-control" id="birth_date"
-                                placeholder="Nascimento">
+                            style="border: 0.5px solid #222; 
+                            color: black;
+                            background: transparent"    
+                            placeholder="Nascimento">
                             <label class="ms-2" for="birth_date">Nascimento</label>
                             @error('birth_date')
                                 <span class="text-danger mt-2">{{ $message }}</span>
@@ -53,7 +67,10 @@
 
                         <div class="col-md-6 form-floating mb-3">
                             <input type="email" wire:model="email" class="form-control" id="email"
-                                placeholder="E-mail">
+                            style="border: 0.5px solid #222; 
+                            color: black;
+                            background: transparent"    
+                            placeholder="E-mail">
                             <label class="ms-2" for="email">E-mail</label>
                             @error('email')
                                 <span class="text-danger mt-2">{{ $message }}</span>
@@ -62,7 +79,10 @@
 
                         <div class="col-md-6 form-floating mb-3">
                             <input type="number" wire:model="phone" class="form-control" id="phone"
-                                placeholder="Telefone">
+                            style="border: 0.5px solid #222; 
+                            color: black;
+                            background: transparent"    
+                            placeholder="Telefone">
                             <label class="ms-2" for="phone">Telefone</label>
                             @error('phone')
                                 <span class="text-danger mt-2">{{ $message }}</span>
@@ -71,7 +91,10 @@
 
                         <div class="col-md-6 form-floating mb-3">
                             <input type="password" wire:model="password" class="form-control" id="password"
-                                placeholder="Palavra-passe">
+                            style="border: 0.5px solid #222; 
+                            color: black;
+                            background: transparent"    
+                            placeholder="Palavra-passe">
                             <label class="ms-2" for="password">Palavra-passe</label>
                             @error('password')
                                 <span class="text-danger mt-2">{{ $message }}</span>
@@ -80,7 +103,10 @@
 
                         <div class="col-md-6 form-floating mb-4">
                             <input type="password" wire:model="confirmpassword" wire:change="check_password"
-                                class="form-control" id="confirmpassword" placeholder="Confirmar Palavra-passe">
+                            style="border: 0.5px solid #222; 
+                            color: black;
+                            background: transparent"    
+                            class="form-control" id="confirmpassword" placeholder="Confirmar Palavra-passe">
                             <label class="ms-2" for="confirmpassword">Confirmar Palavra-passe</label>
                             @error('confirmpassword')
                                 <span class="text-danger mt-2">{{ $message }}</span>
@@ -88,7 +114,11 @@
                         </div>
 
                         <div class="col-md-6 form-floating mb-3">
-                            <select wire:model="access_id" class="form-select" id="access_id">
+                            <select wire:model="access_id" 
+                            style="border: 0.5px solid #222; 
+                            color: black;
+                            background: transparent"
+                            class="form-select" id="access_id">
                                 <option value="">Selecione</option>
                                 @foreach ($accesses as $item)
                                     <option value="{{ $item->id }}">{{ ucwords($item->description) }}</option>
@@ -101,17 +131,17 @@
                         </div>
                     </div>
 
-                    <div class="d-flex flex-column flex-md-row justify-content-between align-items-center mt-3 gap-2">
+                    <div class="d-flex justify-content-between align-items-center mt-2 gap-2">
                         <button type="submit" wire:loading.attr="disabled" wire:target="save"
                             class="btn btn-primary px-4 py-2">
                             <span wire:loading.remove wire:target="save">Cadastrar</span>
                             <span wire:loading wire:target="save">
-                                <i class="fa fa-spinner fa-spin"></i> 
+                                <i class="fa fa-spinner fa-spin"></i> Cadastrar
                             </span>
                         </button>
                     
-                        <small class="text-center mb-0">
-                            Já tem uma conta? <a href="{{ route('pages.login') }}">Entrar</a>
+                        <small class="text-center mb-0 text-dark">
+                           <b> Já tem uma conta? <a href="{{ route('pages.login') }}" class="text-primary fw-bold">Entrar</a></b>
                         </small>
                     </div>
                 </form>
